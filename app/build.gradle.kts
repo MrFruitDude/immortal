761f9f8883d4a6750b3796b7a1a5df3e3b36e1f7
starbrightlab <admin@starbrightlab.com>
2026-06-22 12:45:05 -0400

    Release 1.45 (versionCode 46)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     applicationId = "com.immortal.launcher"
     minSdk = 24
     targetSdk = 36
-    versionCode = 45
-    versionName = "1.44"
+    versionCode = 46
+    versionName = "1.45"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
